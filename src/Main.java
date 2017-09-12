848c4ef219dec55584a4e2ee6e104e173c65db09
KhangarMukeshSingh <31849516+KhangarMukeshSingh@users.noreply.github.com>
2017-09-12 13:15:55 +0530

    2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,17 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+		Student s1=new Student();
+		s1.getId();
+		s1.setId();
+		s1.getFullName();
+		s1.setFullName();
+		s1.getBirthDate();
+		s1.setBirthDate();
+		s1.getAvgMark();
+		s1.setAvgMark();
+		s1.hashCode();
+		
 	}
 
 }
